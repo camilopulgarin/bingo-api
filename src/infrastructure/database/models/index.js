39d806da8d428a9b5b8328e5fc6b8147ee56417c
Andres Pulgarin <andres.pulgarin.nopia@gmail.com>
2024-12-21 16:39:48 -0500

    feat: implementación de post para crear usuarios

--- src/infrastructure/database/models/index.js
+++ src/infrastructure/database/models/index.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 const UserModel = require('./User');
 
-const sequelize = new Sequelize(process.env.DB_URL || 'mysql://user:password@localhost:3306/mydb');
+const sequelize = new Sequelize(process.env.DB_URL || 'mysql://root:root@localhost:3306/bingo_db');
 
 // Inicializar modelos
 const User = UserModel(sequelize);
