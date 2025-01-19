23b2959cbb38bd2b0db3050381eaee1bb59b1023
Andres Pulgarin <andres.pulgarin.nopia@gmail.com>
2025-01-19 18:49:46 -0500

    add migration config

--- src/infrastructure/config/index.js
+++ src/infrastructure/config/index.js
@@ -1,10 +1,9 @@
-
 module.exports = {
-  port: process.env.DB_PORT || 3306,
-  db: {
-    host: process.env.MYSQL_HOST,
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB,
-  },
-};
+    port: process.env.DB_PORT || 3306,
+    db: {
+      host: process.env.MYSQL_HOST,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB,
+    },
+  };
